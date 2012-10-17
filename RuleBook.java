a9e1e8351dd87880be17717f32383e698f33b7d9
Christopher Lindblom <lindblom@doless.se>
2012-10-17 18:34:28 +0200

    added RuleBook class

---
+++ RuleBook.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+/**
+ * RuleBook is a class that handles a collection of Rules.
+ * 
+ * @author Christopher Lindblom
+ * @version 2012-10-17
+ */
+public class RuleBook
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Rule> mRules;
+
+    /**
+     * Constructor for objects of class RuleBook
+     */
+    public RuleBook()
+    {
+        mRules = new ArrayList<Rule>;
+    }
+    
+    /**
+     * Prints all the rules as a list.
+     */
+    public void printRules()
+    {
+        for(Rule rule : mRules)
+        {
+            // prints the rule by using the overridden toString method
+            System.out.println(rule);
+        }
+    }
+    
+    /**
+     * Returns a random rule from the rule book
+     * @return  a random rule
+     */
+    public Rule 
+}
